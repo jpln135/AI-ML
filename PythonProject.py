a8bef4cead321e6a036f835a8ef06df8ba51fa11
J Prabhath <47539168+jpln135@users.noreply.github.com>
2019-02-13 17:18:22 +0530

    Add files via upload

--- PythonProject.py
+++ PythonProject.py
@@ -41,31 +41,42 @@ D = np.array([a, b])
 r = np.linalg.norm(D - A)
 print(r)
 
-
+len = 1000
 t = np.linspace(0, 2*np.pi, 1000)
-x = a + (r*np.cos(t))
-y = b + (r*np.sin(t))
+x = np.zeros((len, 2))
 
-plt.plot(x, y)
-plt.xlabel('$x$')
-plt.ylabel('$y$')
+for i in range(len):
+	cir = np.array([a + r*np.cos(t[i]), b + r*np.sin(t[i])])
+	x[i, :] = cir
 
-x1 = np.linspace(O[0][0] - (r+1), O[0][0] + (r+1),100)
-y1 = 0.25*x1 - 0.75
-plt.plot(x1,y1, label = '$Given line$')
+plt.plot(x[:, 0], x[:, 1], label = '$Circle$')
+
+K = np.array([-1,4])
+N = np.matmul(omat,K)
+k = np.linalg.norm(N)
+y = np.zeros((2, len))
+l = np.linspace(-(r+1)/k,(r+1)/k,len)
+x_O = np.zeros((2, len))
+for j in range(len):
+	line = D + l[j]*N
+	x_O[:,j] = line.T
+
+plt.plot(x_O[0, :], x_O[1, :], label = '$Given - line$')
 
 plt.plot(2,3,'o')
 plt.text(2.1, 3.1, 'A')
 plt.plot(4,5,'o')
 plt.text(4.1, 5.1, 'B')
-plt.text(-0.43,- 2.7, 'radius = 5.84')
+plt.text(-0.43,- 2.7, 'radius = 4.741')
 
 plt.plot(O[0,0],O[1,0],'o')
 plt.text(D[0] - 0.5, D[1] + 0.5, '$Centre$')
 
 plt.axis('equal')
-plt.title('Plot of the Circle')
+plt.title('Plot of the circle')
 plt.legend(loc = 'best')
+
 plt.grid()
 plt.savefig('Circle_Line.png')
 plt.show()
+
